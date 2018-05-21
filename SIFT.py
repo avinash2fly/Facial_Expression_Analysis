1a0e1edb1e1c79179b7d17a43409146071b8e069
Dian Oei <dian.oei.106@gmail.com>
2018-05-21 14:39:24 +1000

    Integrate SIFT to CNN

--- SIFT.py
+++ SIFT.py
@@ -42,36 +42,7 @@ def extractKeyPoints(data):
 	return key_features
 
 
-
-
-# ************************** MAIN CODE ************************** #
-
-if __name__ == "__main__":
-	
-	# -------------------------- Read Images ------------------------ #
-
-	# filepath = os.path.abspath(os.path.join('./Data/fer2013/', 'fer2013.csv'))
-	filepath = os.path.abspath(os.path.join('./', 'fer20131000.csv'))
-
-	dt = pd.read_csv(filepath, sep=',', header=0)
-
-	train_dt = dt.loc[dt['Usage'] == 'Training', :]
-	# train_dt = train_dt[:256]
-	# validation_dt = dt.loc[dt['Usage'] == 'PrivateTest', :]
-	# test_dt = dt.loc[dt['Usage'] == 'PublicTest', :]
-
-	# Convert labels into 1 hot encoding
-	labels = np.zeros((train_dt['emotion'].shape[0], 7))
-	labels[np.arange(labels.shape[0]), train_dt['emotion']] = 1
-
-
-
-	# Data need to be uint8 type or else SIFT/SURF does not work
-	data = [np.array(dtpoint.split(), dtype= np.uint8) for dtpoint in train_dt['pixels']]
-
-
-
-
+def getSiftFeatures(data):
 	# ------------------- Bag of features processing ---------------- #
 
 	# Step 1 : Extract key points and descriptors
@@ -102,6 +73,42 @@ if __name__ == "__main__":
 		anchor += n_kp
 
 	SIFT_data[0] = np.array(SIFT_data[0])
+
+	return SIFT_data
+
+
+
+# ************************** MAIN CODE ************************** #
+
+if __name__ == "__main__":
+
+	# -------------------------- Read Images ------------------------ #
+
+	# filepath = os.path.abspath(os.path.join('./Data/fer2013/', 'fer2013.csv'))
+	filepath = os.path.abspath(os.path.join('./', 'fer20131000.csv'))
+
+	dt = pd.read_csv(filepath, sep=',', header=0)
+
+	train_dt = dt.loc[dt['Usage'] == 'Training', :]
+	# train_dt = train_dt[:256]
+	# validation_dt = dt.loc[dt['Usage'] == 'PrivateTest', :]
+	# test_dt = dt.loc[dt['Usage'] == 'PublicTest', :]
+
+	# Convert labels into 1 hot encoding
+	labels = np.zeros((train_dt['emotion'].shape[0], 7))
+	labels[np.arange(labels.shape[0]), train_dt['emotion']] = 1
+
+
+
+	# Data need to be uint8 type or else SIFT/SURF does not work
+	data = [np.array(dtpoint.split(), dtype= np.uint8) for dtpoint in train_dt['pixels']]
+
+	# Get the SIFT features 
+	SIFT_data = getSiftFeatures(data)
+
+
+
+
 
 
 # scores = {}
